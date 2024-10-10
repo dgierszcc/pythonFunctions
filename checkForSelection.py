9fcb928a8c8695c17adb091a730e738930aa4317
Dan Giersz <90721544+dgierszcc@users.noreply.github.com>
2024-10-10 16:29:11 -0400

    Update checkForSelection.py
    Added comments

--- checkForSelection.py
+++ checkForSelection.py
@@ -1,6 +1,11 @@
-#check for selection
 def check_for_selection(layer_name):
-    """Checks if a layer has any selected features."""
+    """
+    Checks if a layer has any selected features.
+    
+    Args:
+    layer_name (str): Layer to be checked for active selection.
+    """
+    
     desc = arcpy.Describe(layer_name)
     if desc.FIDSet:
         return True
